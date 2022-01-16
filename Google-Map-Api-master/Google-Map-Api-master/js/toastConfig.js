1f5516af1efe7d1571380ae009a3aa165f7d83df
VietTruong-50 <65813680+VietTruong-50@users.noreply.github.com>
2022-01-17 01:02:15 +0700

    Add files via upload

---
+++ Google-Map-Api-master/Google-Map-Api-master/js/toastConfig.js
@@ -0,0 +1,10 @@
+siiimpleToast = siiimpleToast.setOptions({
+    container: "body",
+    class: "siiimpleToast",
+    position: "top|center",
+    margin: 15,
+    delay: 0,
+    duration: 1500,
+    style: {},
+  });
+  
